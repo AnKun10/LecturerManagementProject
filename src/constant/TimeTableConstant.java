a2b9cc7f02a5480f366811971d0e62fbcba0b7c9
AnKun10 <114561027+AnKun10@users.noreply.github.com>
2022-11-29 17:31:10 +0700

    Update TimeTableConstant.java

--- src/constant/TimeTableConstant.java
+++ src/constant/TimeTableConstant.java
@@ -1,8 +1,8 @@
 package constant;
 
 public enum TimeTableConstant {
-    ODDDAYS(""),
-    EVENDAYS("");
+    ODDDAYS("THỨ 3,5,7 (6:30 P.M. -> 9:30 P.M.)"),
+    EVENDAYS("THỨ 2,4,6 (6:30 P.M. -> 9:30 P.M.)");
 
     private String value;
 
