4dc6b415f9ad30fdc13eb93e7771b74d7b5299e9
Minhtamtrinh2303 <114578738+Minhtamtrinh2303@users.noreply.github.com>
2022-11-29 23:32:36 +0700

    Register Function
    decentralization

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -1,6 +1,11 @@
 package utils;
 
+import entity.Lecturer;
+import entity.Person;
+
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Validator {
 
@@ -18,4 +23,47 @@ public class Validator {
         return value;
     }
 
+    public boolean userNameIsValid(String username, ArrayList<Lecturer> lecturers) {
+        // to register account, username cannot be left blank
+        if (username.length() == 0) {
+            System.out.println("Username cannot be left blank, please try again.");
+        }
+        //to register another account, username cannot be available before
+        for (Lecturer lecturer : lecturers) {
+            if (lecturer.getUsername().equalsIgnoreCase(username)) {
+                System.out.println("The Username is available, try again.");
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean emailIsValid(String email, ArrayList<Lecturer> lecturers) {
+        // Use Regex to validate Email
+        String emailPattern =
+                "^[a-zA-Z][\\w-]+@([\\w]+\\.[\\w]+|[\\w]+\\.[\\w]{2,}\\.[\\w]{2,})$";
+        // to register account, the Email cannot be available before
+        for (Lecturer lecturer : lecturers) {
+            if (lecturer.getEmail().equalsIgnoreCase(email)) {
+                System.out.println("The email is available, try again.");
+                return false;
+            }
+        }
+        // valid Email + Email cannot be available before
+        boolean isValid = Pattern.matches(emailPattern, email);
+        if (!isValid) {
+            System.out.println("Email is not valid, try again.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean passIsValid(String password) {
+        // Password Length from 7 to 15 characters
+        // Password has one capital character, one special character
+        String passPattern = "^\\w{7,15}$";
+
+    }
 }
